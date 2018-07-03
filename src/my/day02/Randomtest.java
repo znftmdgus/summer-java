6becbfc6c1a1aa35a7b25dc9476f154a47866c34
414 <414@DESKTOP-8RFB7UD>
2018-07-03 17:04:36 +0900

    asd

---
+++ src/my/day02/Randomtest.java
@@ -0,0 +1,35 @@
+package my.day02;
+
+import java.util.*;
+
+public class Randomtest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Random rand = new Random();
+		int num = rand.nextInt(100);
+		
+		//Scanner scan = new Scanner(System.in);
+		//int i = scan.nextInt();
+		
+		System.out.println("발생한 난수는 " + num);
+		
+		/*if(0<= i) {
+			if(i <100) {
+				
+				switch(i) {
+				case num: System.out.println("정답");
+						break;
+				
+				}
+				System.out.println("입력한 수는 "+ i);
+				System.out.println("발생한 난수는 "+ num);
+			}else {
+				System.out.println("범위 밖의 수입니다.");
+			}		
+		}else {
+		System.out.println("범위 밖의 수입니다.");
+		}*/
+	}
+		
+}
