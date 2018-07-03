7a1293781602e70f0c75fade358aee7784953a3c
414 <414@DESKTOP-8RFB7UD>
2018-07-03 16:33:38 +0900

    asdd

---
+++ src/Varkind.java
@@ -0,0 +1,14 @@
+
+public class Varkind {
+	public static void main(String[] args) {
+		char c = 'a';
+		int s = 5;
+		double d = 1.1;
+		boolean b = true;
+		
+		System.out.println("c는"+c);
+		System.out.println("s는"+s);
+		System.out.println("d는"+d);
+		System.out.println("b는"+b);
+	}
+}
